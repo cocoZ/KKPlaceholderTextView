e6f46db0c71a49431db03d782918860ae1b1461f
cocoa <cocoa.zkk@foxmail.com>
2019-03-05 08:35:17 +0800

    podspec文件完成

--- KKPlaceholderTextView/KKPlaceholderTextView.h
+++ KKPlaceholderTextView/KKPlaceholderTextView.h
@@ -8,6 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+IB_DESIGNABLE
+/**
+ 实现占位文本UITextView, 简单设置文本内边距
+ */
 @interface KKPlaceholderTextView : UITextView
+
+///  内边距
+@property (nonatomic, assign) UIEdgeInsets padding;
+@property (nonatomic, copy) IBInspectable NSString *placeholder;
+@property (nonatomic, copy) IBInspectable NSAttributedString *attributedPlaceholder;
+@property (nonatomic, strong) IBInspectable UIColor *placeholderTextColor;
 
 @end
--- KKPlaceholderTextView/KKPlaceholderTextView.m
+++ KKPlaceholderTextView/KKPlaceholderTextView.m
@@ -8,14 +8,121 @@
 
 #import "KKPlaceholderTextView.h"
 
+@interface KKPlaceholderTextView ()
+
+@property (nonatomic, strong) UILabel *placeholderLabel;
+
+@end
+
 @implementation KKPlaceholderTextView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
-*/
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [self setup];
+}
+- (void)setup {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updatePlaceholderLabelState) name:UITextViewTextDidChangeNotification object:self];
+}
+
+- (void)updatePlaceholderLabelState {
+    _placeholderLabel.hidden = self.hasText;
+    [self setNeedsLayout];
+    [self layoutIfNeeded];
+}
+- (UILabel *)placeholderLabel {
+    if (_placeholderLabel == nil) {
+        _placeholderLabel = [UILabel new];
+        _placeholderLabel.textAlignment = self.textAlignment;
+        _placeholderLabel.numberOfLines = 0;
+        _placeholderLabel.font = self.font;
+        _placeholderLabel.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+        _placeholderLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        _placeholderLabel.textColor = [UIColor lightGrayColor];
+        _placeholderLabel.hidden = YES;
+        [self addSubview:_placeholderLabel];
+    }
+    return _placeholderLabel;
+}
+
+#pragma mark - TextView
+- (void)setText:(NSString *)text {
+    [super setText:text];
+    [self updatePlaceholderLabelState];
+}
+- (void)setAttributedText:(NSAttributedString *)attributedText {
+    [super setAttributedText:attributedText];
+    [self updatePlaceholderLabelState];
+}
+- (void)setFont:(UIFont *)font {
+    [super setFont:font];
+    [self updatePlaceholderLabelState];
+}
+- (void)setTextAlignment:(NSTextAlignment)textAlignment {
+    [super setTextAlignment:textAlignment];
+    _placeholderLabel.textAlignment = textAlignment;
+    [self updatePlaceholderLabelState];
+}
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.placeholderLabel.frame = [self placeholderFrame];
+}
+- (CGSize)intrinsicContentSize {
+    CGSize newSize = [super intrinsicContentSize];
+    if (self.hasText == NO) {
+        UIEdgeInsets padding = self.padding;
+        CGRect placeholderFrame = [self placeholderFrame];
+        newSize.height = placeholderFrame.size.height + padding.top + padding.bottom;
+    }
+    return newSize;
+}
+
+
+#pragma mark - Custom
+
+- (CGRect)placeholderFrame {
+    UIEdgeInsets padding = self.padding;
+    CGFloat maxWidth = CGRectGetWidth(self.frame) - padding.left - padding.right;
+    CGFloat height = CGRectGetHeight(self.frame) - padding.top - padding.bottom;
+    CGSize size = [self.placeholderLabel sizeThatFits:CGSizeMake(maxWidth, height)];
+    
+    return CGRectMake(padding.left, padding.top, size.width, size.height);
+}
+
+- (void)setPlaceholder:(NSString *)placeholder {
+    _placeholder = placeholder;
+    self.placeholderLabel.text = placeholder;
+    [self updatePlaceholderLabelState];
+}
+- (void)setAttributedPlaceholder:(NSAttributedString *)attributedPlaceholder {
+    _attributedPlaceholder = attributedPlaceholder;
+    self.placeholderLabel.attributedText = attributedPlaceholder;
+    [self updatePlaceholderLabelState];
+}
+- (void)setPlaceholderTextColor:(UIColor *)placeholderTextColor {
+    _placeholderTextColor = placeholderTextColor;
+    self.placeholderLabel.textColor = placeholderTextColor;
+}
+
+- (UIEdgeInsets)padding {
+    return UIEdgeInsetsMake(self.textContainerInset.top, self.textContainerInset.left + self.textContainer.lineFragmentPadding, self.textContainerInset.bottom, self.textContainerInset.right + self.textContainer.lineFragmentPadding);
+}
+- (void)setPadding:(UIEdgeInsets)padding {
+    UIEdgeInsets newInset = UIEdgeInsetsMake(padding.top, padding.left - self.textContainer.lineFragmentPadding, padding.bottom, padding.right - self.textContainer.lineFragmentPadding);
+    self.textContainerInset = newInset;
+}
+
+
 
 @end
